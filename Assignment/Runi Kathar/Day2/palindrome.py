4f25237258b222374c82934f10464ad6b531d012
Runi Kathar <runi2023@gift.edu.in>
2024-07-03 00:15:47 +0530

    Update and rename readme.txt to palindrome.py

---
+++ Assignment/Runi Kathar/Day2/palindrome.py
@@ -0,0 +1,11 @@
+sp=40
+for i in range(1,10):
+      for j in range(0,sp):
+        print(' ',end=' ')
+      for j in range(0,(i+1)):
+        print(j,end=' ')
+      for j in range(i-1,0,-1):
+        print(j, end=' ')
+print()
+sp=sp-1
+
