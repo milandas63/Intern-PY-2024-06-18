547eaae5bf642c59c6940eb423823ea89eb68eef
Runi Kathar <runi2023@gift.edu.in>
2024-07-03 00:29:09 +0530

    Update and rename palindrome.py to Diamond.py

---
+++ Assignment/Runi Kathar/Day2/Diamond.py
@@ -0,0 +1,16 @@
+spaces = 40
+stars = 1
+for i in range(0,19):
+	for j in range(0,spaces):
+		print(' ',end='')
+	for j in range(0,stars):
+		print('1',end='')
+	print()
+	if(i<9):
+		spaces = spaces-1
+		stars = stars+2
+	else:
+		spaces = spaces+1
+		stars = stars-2
+
+
