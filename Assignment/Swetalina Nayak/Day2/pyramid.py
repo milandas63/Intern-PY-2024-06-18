5901930e59ca019ec84bf16e73cd3ccfd689ec4c
Swetalinanayak <swetalina2023@gift.edu.in>
2024-07-04 00:43:13 +0530

    Add files via upload

---
+++ Assignment/Swetalina Nayak/Day2/pyramid.py
@@ -0,0 +1,10 @@
+sp = 40
+for i in range(1 ,10):
+    for j in range(0,sp):
+            print(' ',end=' ')
+    for j in range (1 ,(i+1)):
+            print(j,end=' ')
+    for j in range(i-1,0,-1):
+            print(j,end=' ')
+            print()
+            sp = sp-1
