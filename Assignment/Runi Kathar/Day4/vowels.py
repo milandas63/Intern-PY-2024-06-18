06114dc50df64c44df2a0a8cc4007687f5c2c774
Runi Kathar <runi2023@gift.edu.in>
2024-07-04 00:26:27 +0530

    Add files via upload

---
+++ Assignment/Runi Kathar/Day4/vowels.py
@@ -0,0 +1,11 @@
+text = 'Hello Students ,We are learnig python'
+vowels ='AEIOUaeiou'
+
+count=0
+for i in range(0,len(text)):
+    v = text[i]
+    for j in range(0,len(vowels)):
+        if(vowels[j]==v):
+            count=count+1
+            break
+        print('Total number of vowels are',count)
