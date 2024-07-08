e8f9445e98babbfb50d4f1adab405631f4bed318
Purnima Sahoo <purnimasahoo308@gmail.com>
2024-07-08 09:55:35 +0530

    Add files via upload

---
+++ Assignment/Purnima Sahoo/day-03/hollowpyramid.py
@@ -0,0 +1,16 @@
+spaces = 40
+stars = 1
+charac = '*'
+
+for i in range(0,10):
+	for j in range(0,spaces):
+		print(' ',end='')
+	for j in range(0,stars):
+		if j==0 or j==(stars-1) or i==9:
+			charac = '*'
+		else:
+			charac = ' '
+		print(charac,end='')
+	print()
+	spaces = spaces-1
+	stars = stars+2
