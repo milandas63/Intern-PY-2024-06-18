9e4edae2db4ed2775e07c94902574d83a61120af
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-07-11 15:15:13 +0530

    Add files via upload

--- Day-17 (11-07-2024)/db_repo.py
+++ Day-17 (11-07-2024)/db_repo.py
@@ -26,3 +26,22 @@ for index, item in enumerate(list):
     print(s.replicate("-",item[0]), end='+')
 print()
 
+i = 0;
+for row in curs:
+	print('|',end='')
+	position = list[i]
+	for index, col in enumerate(row):
+		list_index = list[index]
+		width = list_index[0]
+		print(s.justL(col,width), end='|')
+
+	print()
+	i = i+1
+
+print("+",end='')
+for index, item in enumerate(list):
+    print(s.replicate("-",item[0]), end='+')
+print()
+
+
+
