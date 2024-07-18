a49c9c41a27577146ae9b4f17aedcc7f9993f9d0
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-07-18 13:57:40 +0530

    Add files via upload

---
+++ Day-21 (18-07-2024)/option_menu.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+root = Tk()
+root.title('Dropdown Box')
+root.iconbitmap('images/bird.ico')
+root.geometry('500x340')
+
+options = (
+	'COBOL',
+	'BASIC',
+	'Algol',
+	'Pascal',
+	'ForTran',
+	'Java',
+	'Python',
+	'JavaScript',
+	'C++'
+)
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop_down = OptionMenu(root, clicked, *options)
+drop_down.pack()
+
+lbl_result = Label(root, text='...')
+
+def show():
+	global lbl_result
+	global clicked
+	lbl_result.config(text=clicked.get())
+
+btn_click = Button(root, text='Click to view the choice', command=show).pack(pady=10)
+
+lbl_result.pack(pady=40)
+
+root.mainloop()
