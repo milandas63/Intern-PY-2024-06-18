deabbce581c5950e881bee4dc2be86507f9876b4
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-07-18 14:31:53 +0530

    Delete EOFErr.py

--- EOFErr.py
+++
@@ -1,7 +0,0 @@
-def eof_error_example():
-    try:
-        input("Press Ctrl+D (or Ctrl+Z on Windows) to trigger EOFError: ")
-    except EOFError as e:
-        print(f"EOFError caught: {e}")
-
-eof_error_example()
