031f9ec408f9a266f7012d3d93f421ce57d75d1f
AryanHBehera <159294464+AryanHBehera@users.noreply.github.com>
2024-07-25 14:42:37 +0530

    Delete PROJECT/Group 01/CODE directory

--- PROJECT/Group 01/CODE/alternative_detection.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-def detect_faces_haar(image_path):
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-    return faces, image
--- PROJECT/Group 01/CODE/display_results.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def display_image_with_boxes(image):
-    # Convert the image from BGR to RGB for display
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    plt.imshow(image_rgb)
-    plt.axis('off')
-    plt.show()
