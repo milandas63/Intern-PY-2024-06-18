68938e439c78d5022b6c59c2ba8fccef64d8aa2b
Runi Kathar <runi2023@gift.edu.in>
2024-08-10 11:53:04 +0530

    Add files via upload

---
+++ Assignment/Runi Kathar/Day-21/GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Python GUI Applicatin")
+
+root.geometry("600x400")
+Label=Label(root,text="Hello world",font='40',bg='yellow',fg='red')
+Label.grid()
+root.mainloop() 
